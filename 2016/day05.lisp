a92fd86d432b789c8f1f6e7188490999d186f580
Johan Andersson <nilsjohanandersson@gmail.com>
2022-09-18 16:40:53 +0200

    Improve day 05 2016

--- 2016/day05.lisp
+++ 2016/day05.lisp
@@ -2,13 +2,24 @@
 
 (in-package #:aoc2016.day05)
 
-(defun octets (string)
-  (map '(simple-array (unsigned-byte 8) (*))
-       #'char-code
-       string))
+(defun tick (vector p)
+  (let ((i (- (length vector) p 1)))
+    (cond ((<= (char-code #\0) (aref vector i) (char-code #\8))
+           (incf (aref vector i))
+           vector)
+          ((= (char-code #\9) (aref vector i))
+           (setf (aref vector i) (char-code #\0))
+           (tick vector (1+ p)))
+          (t (tick (grow vector p) p)))))
 
-(defun md5 (string)
-  (ironclad:digest-sequence :md5 (octets string)))
+(defun grow (vector p)
+  (loop with grown = (make-array (1+ (length vector)) :element-type '(unsigned-byte 8))
+        with x = (- (length vector) p)
+        for i below (length grown)
+        do (setf (aref grown i) (cond ((< i x) (aref vector i))
+                                      ((= i x) (char-code #\0))
+                                      (t (aref vector (1- i)))))
+        finally (return grown)))
 
 (defun hi (octet)
   (declare (type (integer 0 255) octet))
@@ -18,6 +29,9 @@
   (declare (type (integer 0 255) octet))
   (the (integer 0 15) (logand #x0f octet)))
 
+(defun octets (string)
+  (map '(vector (unsigned-byte 8)) #'char-code string))
+
 (defun interesting (hash)
   (when (= 0
            (aref hash 0)
@@ -26,37 +40,35 @@
     (values (lo (aref hash 2))
             (hi (aref hash 3)))))
 
-(defun concat (string i)
-  (concatenate 'string string (format nil "~d" i)))
-
 (defun find-matches (n input)
   (loop with count = 0
-        for i upfrom 0
-        for val = (interesting (md5 (concat input i)))
+        for buf = (grow input 0) then (tick buf 0)
+        for val = (interesting (ironclad:digest-sequence :md5 buf))
         when val
           collect val
           and do (incf count)
         until (= count n)))
 
 (defun hex-digit (i)
-  (declare (type (integer 0 15) i))
-  (char (format nil "~x" i) 0))
+  (digit-char i 16))
 
 (defun password (values)
   (map 'string #'hex-digit values))
 
 (defun part1 (input)
-  (password (find-matches 8 input)))
+  (password (find-matches 8 (octets input))))
 
 (defun set-matches (n input)
   (loop with password = (make-sequence 'string n :initial-element #\_)
-        for i upfrom 0
+        with digest = (ironclad:make-digest :md5)
+        for buf = (grow input 0) then (tick buf 0)
         for (pos val) = (multiple-value-list
-                         (interesting (md5 (concat input i))))
+                         (interesting (ironclad::digest-sequence digest buf)))
+        do (ironclad::reinitialize-instance digest)
         when (and val (< pos n) (char= #\_ (char password pos)))
           do (setf (char password pos) (hex-digit val))
         until (not (find #\_ password))
         finally (return password)))
 
 (defun part2 (input)
-  (set-matches 8 input))
+  (set-matches 8 (octets input)))
