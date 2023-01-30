7c076dced9e549f11610800dc267537739e9a113
Johan Andersson <nilsjohanandersson@gmail.com>
2023-01-30 21:01:12 +0100

    Day 13 2019

--- package.lisp
+++ package.lisp
@@ -168,6 +168,7 @@
 (defpackage :aoc2019.day10 (:use :cl) (:local-nicknames (:a :alexandria)))
 (defpackage :aoc2019.day11 (:use :cl) (:local-nicknames (:a :alexandria)))
 (defpackage :aoc2019.day12 (:use :cl) (:local-nicknames (:a :alexandria)))
+(defpackage :aoc2019.day13 (:use :cl) (:local-nicknames (:a :alexandria)))
 (fiasco:define-test-package :aoc2019.tests (:use :aoc))
 
 (defpackage :aoc2020.day01 (:use :cl) (:import-from :alexandria :curry))
