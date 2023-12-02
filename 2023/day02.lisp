dd2a5842c72003910600307d6178a7979bd5ab76
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-02 09:50:01 +0100

    Day 02 part 2 wip

--- 2023/day02.lisp
+++ 2023/day02.lisp
@@ -2,6 +2,9 @@
 
 (in-package :aoc2023.day02)
 
+(defun games (input)
+  (mapcar #'parse-line (aoc:lines input)))
+
 (defun parse-line (line)
   (destructuring-bind (id games) (str:split ":" line)
     (cons (parse-integer (remove-if-not #'digit-char-p id))
@@ -21,7 +24,7 @@
   (every (a:curry #'valid-draw? bag) (draws game)))
 
 (defun cube-count (bag color)
-  (car (rassoc color bag)))
+  (or (car (rassoc color bag)) 0))
 
 (defun valid-draw? (bag draw)
   (loop for (count . color) in draw
@@ -29,10 +32,22 @@
 
 (defun part1 (input)
   (reduce #'+ (mapcar #'id (remove-if-not (a:curry #'possible? '((12 . red) (13 . green) (14 . blue)))
-                                          (mapcar #'parse-line (aoc:lines input))))))
+                                          (games input)))))
 
-(defparameter *test* "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+(defun colors (game)
+  (remove-duplicates (mapcan (lambda (draw) (mapcar #'cdr draw))
+                             (draws game))))
+
+(defun color-maximum (game color)
+  (reduce #'max (mapcar (a:rcurry #'cube-count color) (draws game))))
+
+(defun minimum-set (game)
+  (mapcar (lambda (color)
+            (cons (color-maximum game color) color))
+          (colors game)))
+
+(defun power (bag)
+  (reduce #'* (mapcar #'car bag)))
+
+(defun part2 (input)
+  (reduce #'+ (mapcar (a:compose #'power #'minimum-set) (games input))))
