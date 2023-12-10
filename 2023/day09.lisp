60941edf883e35c9d7d27abbd7574dd3be0909f4
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-10 08:16:23 +0100

    Day 09

--- 2023/day09.lisp
+++ 2023/day09.lisp
@@ -2,25 +2,29 @@
 
 (in-package :aoc2023.day09)
 
-(defun parse-line (line)
-  (mapcar #'parse-integer (str:split " " line)))
-
 (defun parse (input)
-  (mapcar #'parse-line (aoc:lines input)))
-
-(defparameter *test*
-"0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45")
+  (mapcar (a:compose (a:curry #'mapcar #'parse-integer)
+                     (a:curry #'str:split " "))
+          (aoc:lines input)))
 
 (defun differences (numbers)
   (mapcar (lambda (a b) (- b a)) numbers (cdr numbers)))
+
+(defun predictions (numbers)
+  (let ((differences (differences numbers)))
+    (if (every #'zerop differences)
+        (values (a:lastcar numbers) (first numbers))
+        (multiple-value-bind (last first) (predictions differences)
+          (values (+ (a:lastcar numbers) last) (- (first numbers) first))))))
+
+(defun part1 (input)
+  (reduce #'+ (mapcar #'prediction (parse input))))
+
+(defun part2 (input)
+  (reduce #'+ (mapcar (lambda (n) (nth-value 1 (predictions n))) (parse input))))
 
 (defun prediction (numbers)
   (let ((differences (differences numbers)))
     (if (every #'zerop differences)
         (a:lastcar numbers)
         (+ (a:lastcar numbers) (prediction differences)))))
-
-(defun part1 (input)
-  (reduce #'+ (mapcar #'prediction (parse input))))
