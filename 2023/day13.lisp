447bf2a87effe21d5e4a730bcb97ac06d626ba1c
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-13 09:57:42 +0100

    Day 13 part2 (broke part1)

--- 2023/day13.lisp
+++ 2023/day13.lisp
@@ -17,8 +17,8 @@
              (and (rest pair)
                   
 ;                  (prog1 (print pair) (terpri))
-                  (loop for l downfrom (apply #'min pair)
-                        for r from (apply #'max pair)
+                  (loop for l downfrom (apply #'max pair)
+                        for r from (apply #'min pair)
                         for ll = (gethash l lookup)
                         for lr = (gethash r lookup)
                         while (and ll lr)
@@ -32,8 +32,9 @@
     (dolist (v (a:hash-table-values hash-table) result)
       (when (rest v)
         (a:map-combinations (lambda (x)
-                              (when (= 1 (- (apply #'max x) (apply #'min x)))
-                                (push x result)))
+                              ;; (when (= 1 (- (apply #'max x) (apply #'min x)))
+                              ;;   (push x result)))
+                              (push x result))
                             v :length 2)))))
 
 (defun print-hash-table (hash-table)
@@ -69,23 +70,71 @@
 (defun part1 (input)
   (reduce #'+ (mapcar #'score (aoc:sections input))))
 
-;; 2023 13 1: '26774'
-;; That's not the right answer.
-;; Please wait one minute before trying again.
-;; (Cached until 2023-12-13 06:53:01)
+(defun smudge? (a b)
+  (= 1 (loop for x across a
+             for y across b
+             count (not (char= x y)))))
 
-;; 2023 13 1: '28490'
+(defun line-of-reflection* (hash-table)
+  (let ((lookup (reverse-hash hash-table)))
+    ;; (terpri)
+    ;; (print-hash-table lookup)
+    (flet ((reflects (pair)
+             (and (rest pair)
+                  
+;                  (prog1 (print pair) (terpri))
+                  (loop for l downfrom (apply #'min pair)
+                        for r from (apply #'max pair)
+                        for ll = (gethash l lookup)
+                        for lr = (gethash r lookup)
+                        for count from 0
+                        while (and ll lr)
+;                        do (format t "~3d:~%~3d ~a~%~3d ~a~%" count l ll r lr)
+                        count (equal ll lr) into equals
+                        count (smudge? ll lr) into smudges
+                        finally  (return (and (eql (1- count) equals)
+                                              (eql 1 smudges)
+                                              pair))))))
+      (let ((pair (first (remove-if-not #'reflects (smudge-pairs lookup)))))
+        (and pair (apply #'max pair))))))
+
+(defun smudge-pairs (lookup)
+  (let (result)
+    (a:map-combinations
+     (lambda (x)
+       (destructuring-bind ((ai a) (bi b)) x
+         (when (and (= 1 (- (max ai bi) (min ai bi)))
+                    (or (equal a b)
+                        (smudge? a b)))
+           (push (list ai bi) result))))
+     (mapcar (lambda (i) (list i (gethash i lookup)))
+             (a:hash-table-keys lookup))
+     :length 2)
+    result))
+
+
+(defun score* (section)
+  (let ((vertical (line-of-reflection* (hash (transpose (aoc:lines section)))))
+        (horizontal (line-of-reflection* (hash (aoc:lines section)))))
+    ;; (unless (or vertical horizontal)
+    ;;   (format t "~a~%~%" section))
+    (+ (or vertical 0) (* 100 (or horizontal 0)))))
+
+
+(defun part2 (input)
+  (reduce #'+ (mapcar #'score* (aoc:sections input))))
+
+;; 2023 13 2: '14516'
 ;; That's not the right answer; your answer is too low.
 ;; Please wait one minute before trying again.
-;; (Cached until 2023-12-13 07:30:31)
+;; (Cached until 2023-12-13 08:15:16)
 
-;; 2023 13 1: '28504'
-;; That's not the right answer; your answer is too low.
-;; Please wait one minute before trying again.
-;; (Cached until 2023-12-13 07:35:57)
+;; 2023 13 2: '31974'
+;; That's the right answer!
+;; (Cached until 2023-12-13 08:30:04)
 
 (defparameter *test*
-"#.##..##.
+  "#.##..##.
 ..#.##.#.
 ##......#
 ##......#
