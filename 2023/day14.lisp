ce83d5c4e4a38872621f2f88b0beb87dc3ef0676
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-14 07:36:21 +0100

    Day 14 part2 wip

--- 2023/day14.lisp
+++ 2023/day14.lisp
@@ -24,8 +24,26 @@ O.#..O.#.#
 
 (defun tilt-north (array)
   (loop for col below (array-dimension array 1)
-        do (tilt-north-col array col)
-        finally (return array)))
+        do (tilt* array 0 1 col 0)))
+
+(defun tilt-south (array)
+  (loop for col below (array-dimension array 1)
+        do (tilt* array (1- (array-dimension array 0)) -1 col 0)))
+
+(defun tilt-east (array)
+  (loop for row below (array-dimension array 0)
+        do (tilt* array row 0 (1- (array-dimension array 1)) -1)))
+
+(defun tilt-west (array)
+  (loop for row below (array-dimension array 0)
+        do (tilt* array row 0 0 1)))
+
+(defun cycles (array n)
+  (dotimes (x n array)
+    (tilt-north array)
+    (tilt-west array)
+    (tilt-south array)
+    (tilt-east array)))
 
 (defun tilt-north-col (array col)
   (flet ((free? (c) (char= #\. c))
@@ -41,6 +59,26 @@ O.#..O.#.#
                     (setf free nil))
                    ((and (not free) (free? c))
                     (setf free row))))))
+
+(defun tilt* (array start-row rd start-col cd)
+  (flet ((free? (c) (char= #\. c))
+         (round? (c) (char= #\O c))
+         (square? (c) (char= #\# c)))    
+    (loop with fr and fc
+          for row = start-row then (+ row rd)
+          for col = start-col then (+ col cd)
+          while (array-in-bounds-p array row col)
+          for c = (aref array row col)
+          do (cond ((and fr (round? c))
+                    (rotatef (aref array row col) (aref array fr fc))
+                    (incf fr rd)
+                    (incf fc cd))
+                   ((or (round? c) (square? c))
+                    (setf fr nil
+                          fc nil))
+                   ((and (not fr) (free? c))
+                    (setf fr row
+                          fc col))))))
 
 (defun total-load (array)
   (let ((rows (array-dimension array 0)))
