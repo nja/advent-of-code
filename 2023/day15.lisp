d5f17ccb4ca2c4517eab0f40a7bf7ea84e56e1c6
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-15 07:14:05 +0100

    Day 15

--- 2023/day15.lisp
+++ 2023/day15.lisp
@@ -2,14 +2,8 @@
 
 (in-package :aoc2023.day15)
 
-(defparameter *test* "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
-
 (defun hash (string)
-  (loop with hash fixnum = 0
-        for char across string
-        for code = (char-code char)
-        do (setf hash (mod (* 17 (+ hash code)) 256))
-        finally (return hash)))
+  (reduce (lambda (h c) (mod (* 17 (+ h (char-code c))) 256)) string :initial-value 0))
 
 (defun part1 (input)
   (reduce #'+ (mapcar #'hash (str:split "," (remove #\Newline input)))))
@@ -26,13 +20,11 @@
     (let ((i (hash label)))
       (symbol-macrolet ((box (aref boxes i)))
         (case op
-          (- (setf box (remove label box :test #'string= :key #'label)))
-          (= (let ((lens (find label box :test #'string= :key #'label)))
+          (- (setf box (remove label box :test #'string= :key #'first)))
+          (= (let ((lens (find label box :test #'string= :key #'first)))
                (if lens
                    (setf (second lens) focal-length)
                    (push (list label focal-length) box)))))))))
-
-(defun label (x) (first x))
 
 (defun focusing-power (boxes)
   (loop for box-number from 1
