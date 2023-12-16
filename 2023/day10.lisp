4902776cff8af02d2d235ad3ffe67db6b26fca4f
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-16 09:41:35 +0100

    Day 10

--- 2023/day10.lisp
+++ 2023/day10.lisp
@@ -35,115 +35,50 @@
   (let ((array (aoc:to-array input)))
     (/ (length (pipe array (start-position array) 'S)) 2)))
 
-(defun count-insides (array pipes)
+(defun count-insides (array pipe?)
   (loop for row below (array-dimension array 0)
-        sum (loop with inside
-                  for col below (array-dimension array 1)
-                  for p = (find (list row col) pipes :test #'equalp)
-                  when p
-                    do (setf inside (not inside))
-                  count (and (not p) inside))))
+        sum (loop for col below (array-dimension array 1)
+                  for p = (and (funcall pipe? row col)
+                               (aref array row col))
+                  for state = (state 'out p) then (state state p)
+                  count (and (not p) (eq state 'in)))))
 
-(defun embiggen (input)
-  (let ((array (aoc:to-array input)))
-    (mark-big-loop array (start-position array) 'S)))
+(defun state (state pipe)
+  (if (not pipe)
+      state
+      (case state
+        (out (ecase pipe
+               ((#\| #\S) 'in)
+               (#\F 'out-F)
+               (#\L 'out-L)))
+        (in (ecase pipe
+              ((#\| #\S) 'out)
+              (#\F 'in-F)
+              (#\L 'in-L)))
+        (out-F (ecase pipe
+                 (#\- 'out-F)
+                 (#\J 'in)
+                 (#\7 'out)))
+        (out-L (ecase pipe
+                 (#\- 'out-L)
+                 (#\J 'out)
+                 (#\7 'in)))
+        (in-F (ecase pipe
+                (#\- 'in-F)
+                (#\J 'out)
+                (#\7 'in)))
+        (in-L (ecase pipe
+                (#\- 'in-L)
+                (#\J 'in)
+                (#\7 'out))))))
 
-(defun mark-big-loop (array position direction)
-  (loop with copy = (make-array (mul (add (array-dimensions array) '(2 2)) 3) :initial-element #\.)
-        for p = (add position (dir direction)) then (add p (dir dir))
-        for pipe = (apply #'aref array p)
-        for dir = (turn pipe (or dir direction))
-        do (write-big-pipe copy p pipe)
-        when (null dir)
-          return copy))
-
-(defun mul (a f)
-  (mapcar (a:curry #'* f) a))
-
-(defun write-big-pipe (big-array pos pipe)
-  (let ((pos (mul (add pos '(1 1)) 3))
-        (src (getf *big-pipes* pipe)))
-    (loop for big-row from (first pos)
-          for src-row below 3
-          do (loop for big-col from (second pos)
-                   for src-col below 3
-                   do (setf (aref big-array big-row big-col)
-                            (aref src src-row src-col))))))
-
-(defparameter *big-array* nil)
-
-(defun neighbours (pos)
-  (destructuring-bind (row col) pos
-    (let (result)
-      (flet ((collect (r c)
-               (when (and (array-in-bounds-p *big-array* r c)
-                          (find (aref *big-array* r c) "._"))
-                 (push (list r c) result))))
-        (collect (1- row) col)
-        (collect row (1- col))
-        (collect (1+ row) col)
-        (collect row (1+ col))
-        result))))
-
-(defparameter *big-pipes*
-  (list #\| (aoc:to-array
-             "_|_
-_|_
-_|_")
-        #\- (aoc:to-array
-             "___
----
-___")
-        #\L (aoc:to-array
-             "_L_
-_LL
-___")
-        #\J (aoc:to-array
-             "_J_
-JJ_
-___")
-        #\7 (aoc:to-array
-             "___
-77_
-_7_")
-        #\F (aoc:to-array
-             "___
-_FF
-_F_")
-        #\S (aoc:to-array
-             "___
-_SS
-_S_")))
-
-(defun outside-count (array)
-  (let* ((*big-array* (mark-big-loop array (start-position array) 'S))
-         (positions (mapcar #'dijkstra:item (dijkstra:search* '(0 0) #'neighbours))))
-    (- (/ (count-if (lambda (p)
-                      (eql #\. (apply #'aref *big-array* p)))
-                    positions)
-          9)
-       (* 2 (array-dimension array 0))
-       (* 2 (array-dimension array 1))
-       4)))
-
-(defparameter *test2*
-".F----7F7F7F7F-7....
-.|F--7||||||||FJ....
-.||.FJ||||||||L7....
-FJL7L7LJLJ||LJ.L-7..
-L--J.L7...LJS7F-7L7.
-....F-J..F7FJ|L7L7L7
-....L7.F7||L7|.L7L7|
-.....|FJLJ|FJ|F7|.LJ
-....FJL-7.||.||||...
-....L---J.LJ.LJLJ...")
-
-;; (defun part2 (input)
-;;   (let* ((array (aoc:to-array input))
-;;          (distance (length (distance array (start-position array) 'S)))
-;;          (outside (outside-count array)))
-;;     (- (array-total-size array) distance outside)))
+(defun pipe-predicate (pipe)
+  (let ((set (make-hash-table :test 'equal)))
+    (dolist (p pipe)
+      (setf (gethash p set) t))
+    (lambda (&rest key)
+      (gethash key set))))
 
 (defun part2 (input)
   (let ((array (aoc:to-array input)))
-    (count-insides array (pipe array (start-position array) 'S))))
+    (count-insides array (pipe-predicate (pipe array (start-position array) 'S)))))
