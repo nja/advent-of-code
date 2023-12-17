267bbb3beb440c6d2171552f7277ce9e436c3937
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-17 22:07:46 +0100

    Day 17 part2

--- 2023/day17.lisp
+++ 2023/day17.lisp
@@ -7,26 +7,23 @@
     (dotimes (i (array-total-size array) array)
       (setf (row-major-aref array i) (digit-char-p (row-major-aref array i))))))
 
-(defparameter *array* nil)
+(defstruct (state :conc-name) row col dir n)
 
-(defstruct (state :conc-name) row col dir dircount)
-
-(defun neighbours (valid-direction? &optional min-count done?)
+(defun neighbours (array valid-direction? done? &optional min)
   (lambda (state)
     (loop for dir in '(north east south west)
           for next = (make-state :row (+ (row state) (case dir (north -1) (south 1) (t 0)))
                                  :col (+ (col state) (case dir (west -1) (east 1) (t 0)))
                                  :dir dir
-                                 :dircount (if (eq (dir state) dir)
-                                               (1+ (dircount state))
+                                 :n (if (eq (dir state) dir)
+                                               (1+ (n state))
                                                1))
-          when (and (funcall valid-direction? (dir state) (dircount state) dir)
-                    (array-in-bounds-p *array* (row next) (col next))
-                    (or (null min-count)
+          when (and (funcall valid-direction? (dir state) (n state) dir)
+                    (array-in-bounds-p array (row next) (col next))
+                    (or (null min)
                         (not (funcall done? next))
-                        (<= min-count (dircount next))))
+                        (<= min (n next))))
             collect next)))
-
 
 (defun done? (array)
   (destructuring-bind (row col) (array-dimensions array)
@@ -34,31 +31,32 @@
       (and (eql (1- row) (row state))
            (eql (1- col) (col state))))))
 
-(defun valid-direction? (from count dir)
+(defun valid-direction? (from n dir)
   (cond ((null from) t)
-        ((eq from dir) (< count 3))
+        ((eq from dir) (< n 3))
         (t (not (eq dir (case from (north 'south) (east 'west) (south 'north) (west 'east)))))))
 
-(defun distance (src dst)
-  (declare (ignore src))
-  (aref *array* (row dst) (col dst)))
+(defun distance (array)
+  (lambda (src dst)
+    (declare (ignore src))
+    (aref array (row dst) (col dst))))
 
 (defun least-heat-loss (array neighbours)
-  (let* ((*array* array)
-         (n (dijkstra:search* (make-state :row 0 :col 0 :dircount 0)
-                              neighbours
-                              :distancef #'distance
-                              :donep (done? array))))
-    (dijkstra:distance n)))
+  (dijkstra:distance (dijkstra:search* (make-state :row 0 :col 0 :n 0)
+                                       neighbours
+                                       :distancef (distance array)
+                                       :donep (done? array))))
 
 (defun part1 (input)
-  (least-heat-loss (parse input) (neighbours #'valid-direction?)))
+  (let ((array (parse input)))
+    (least-heat-loss array (neighbours array #'valid-direction? (done? array)))))
 
-(defun ultra-valid-direction? (from count dir)
+(defun ultra-valid-direction? (from n dir)
   (cond ((null from) t)
-        ((< count 4) (eq from dir))
-        ((eq from dir) (< count 10))
+        ((< n 4) (eq from dir))
+        ((eq from dir) (< n 10))
         (t (not (eq dir (case from (north 'south) (east 'west) (south 'north) (west 'east)))))))
 
 (defun part2 (input)
-  (least-heat-loss (parse input) (neighbours #'ultra-valid-direction? 4 (done? (parse input) ))))
+  (let ((array (parse input)))
+    (least-heat-loss array (neighbours array #'ultra-valid-direction? (done? array) 4))))
