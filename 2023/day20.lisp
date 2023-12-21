0f6cef1686d97db8ac426b9b4e518b9f5c6ea1c0
Johan Andersson <nilsjohanandersson@gmail.com>
2023-12-21 06:57:19 +0100

    Day 20 part2

--- 2023/day20.lisp
+++ 2023/day20.lisp
@@ -2,20 +2,6 @@
 
 (in-package :aoc2023.day20)
 
-(defparameter *test*
-"broadcaster -> a, b, c
-%a -> b
-%b -> c
-%c -> inv
-&inv -> a")
-
-(defparameter *test2*
-"broadcaster -> a
-%a -> inv, con
-&inv -> b
-%b -> con
-&con -> output")
-
 (defun parse (input)
   (let ((*package* (symbol-package 'parse)))
     (mapcar (lambda (line)
@@ -30,11 +16,11 @@
 
 (defstruct (module :conc-name) id mtype state dests)
 
-(defun modules (modules)
+(defun modules (list)
   (let ((hash (make-hash-table)))
-    (loop for module in modules
+    (loop for module in list
           do (setf (gethash (id module) hash) module))
-    (loop for src in modules do
+    (loop for src in list do
       (loop for dstid in (dests src)
             for dst = (gethash dstid hash)
             when (and dst (eq 'conjunction (mtype dst)))
@@ -43,43 +29,56 @@
 
 (defun receive (module source signal output)
   (flet ((send (sig)
-           (map nil (lambda (dst) (funcall output (id module) sig dst)) (dests module))))
+           (dolist (dst (dests module))
+             (funcall output (id module) sig dst))))
     (case (mtype module)
       (broadcaster (send signal))
       (flip-flop (when (eq signal 'low)
                    (setf (state module) (not (state module)))
                    (send (if (state module) 'high 'low))))
       (conjunction
-                                        ;(format t "~a state was ~a" (id module) (state module))
        (setf (getf (state module) source) signal)
-                                        ;(format t " is ~a~%" (state module))
        (send (if (find 'low (state module))
                  'high
                  'low))))))
 
-(defun process (modules wire count)
+(defun process (modules wire buttons &optional cycles)
   (loop with highs = 0 and lows = 0
         for (src sig dst) = (queues:qpop wire)
         while sig
-        do ;(format t "~a -~a-> ~a~%" src sig dst)
-           (incf count)
-           (case sig
+        do (case sig
              (high (incf highs))
              (low (incf lows)))
+           (a:when-let (bs (and cycles (eq sig 'low) (gethash src cycles)))
+             (when (/= (car bs) buttons)
+               (push buttons (gethash src cycles))))
            (a:when-let (receiver (gethash dst modules))
              (receive receiver src sig
                       (lambda (&rest pulse) (queues:qpush wire pulse))))
-        finally (return (list lows highs count))))
-
-(defun wire ()
-  (queues:make-queue :simple-queue))
+        finally (return (list lows highs))))
 
 (defun part1 (input)
   (let ((mods (modules (parse input)))
-        (wire (wire)))
-    (loop for (lows highs tot) = (progn (queues:qpush wire '(button low broadcaster))
-                                   (process mods wire (or tot 0)))
+        (wire (queues:make-queue :simple-queue)))
+    (loop for buttons from 1
+          for (lows highs) = (progn (queues:qpush wire '(button low broadcaster))
+                                    (process mods wire buttons))
           sum lows into totlows
           sum highs into tothighs
           repeat 1000
           finally (return (* totlows tothighs)))))
+
+(defun part2 (input)
+  (let ((mods (modules (parse input)))
+        (wire (queues:make-queue :simple-queue))
+        (cycles (make-hash-table)))
+    (loop for mod in (a:hash-table-values mods)
+          when (and (eq 'conjunction (mtype mod)) (< 1 (length (dests mod))))
+            do (setf (gethash (id mod) cycles) (list 0)))
+    (loop for buttons from 1
+          do (queues:qpush wire '(button low broadcaster))
+             (process mods wire buttons cycles)
+          while (loop for l being the hash-values in cycles
+                      thereis (< (length l) 2))
+          finally (return (reduce #'lcm (mapcar (lambda (l) (- (first l) (second l)))
+                                                (print (a:hash-table-values cycles))))))))
