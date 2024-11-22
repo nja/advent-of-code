fcf0cf8cdd1cb2253f566e5e5baa97500e7cf5fb
Johan Andersson <nilsjohanandersson@gmail.com>
2024-11-22 16:34:22 +0100

    Day 14 cleanup

--- 2023/day14.lisp
+++ 2023/day14.lisp
@@ -2,18 +2,6 @@
 
 (in-package :aoc2023.day14)
 
-(defparameter *test*
-"O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....")
-
 (defun tilt-north (array)
   (loop for col below (array-dimension array 1)
         do (tilt* array 0 1 col 0)))
@@ -31,14 +19,6 @@ O.#..O.#.#
 (defun tilt-west (array)
   (loop for row below (array-dimension array 0)
         do (tilt* array row 0 0 1)))
-
-(defun cycles (array n)
-  (dotimes (x n array)
-    (declare (type fixnum n x))
-    (tilt-north array)
-    (tilt-west array)
-    (tilt-south array)
-    (tilt-east array)))
 
 (defun tilt* (array start-row rd start-col cd)
   (flet ((free? (c) (char= #\. c))
@@ -72,31 +52,12 @@ O.#..O.#.#
     (total-load array)))
 
 (defun cycle-hash (array)
-  (let ((copy (a:copy-array array)))
-    (loop for i from 1
-          with hash = (make-hash-table :test 'equalp)
-          do (tilt-north copy)
-             (tilt-west copy)
-             (tilt-south copy)
-             (tilt-east copy)
-          when (< 100 i)
-            do (push i (gethash (a:copy-array copy) hash))
-          repeat 200
-          finally (return hash))))
-
-
-(defun load-hash (array)
-  (let ((copy (a:copy-array array)))
-    (loop for i from 1
-          with hash = (make-hash-table)
-          do (tilt-north copy)
-             (tilt-west copy)
-             (tilt-south copy)
-             (tilt-east copy)
-          do (push i (gethash (total-load copy) hash))
-                                        ;until (< 10 (length (gethash (total-load copy) hash)))
-             repeat 36
-          finally (return hash))))
+  (loop with hash = (make-hash-table :test 'equalp)
+        for i from 1
+        for copy = (cycles array 1) then (cycles copy 1)
+        do (push i (gethash copy hash))
+        repeat 128
+        finally (return hash)))
 
 (defun skip-cycles (hash n)
   (let ((ramp-up (ramp-up hash))
@@ -111,7 +72,14 @@ O.#..O.#.#
 (defun ramp-up (hash)
   (count-if-not #'cdr (a:hash-table-values hash)))
 
+(defun cycles (array n)
+  (let ((copy (a:copy-array array)))
+    (dotimes (x n copy)
+      (tilt-north copy)
+      (tilt-west copy)
+      (tilt-south copy)
+      (tilt-east copy))))
+
 (defun part2 (input)
   (let ((array (aoc:to-array input)))
-    (total-load (cycles array (skip-cycles (load-hash array) 1000000000)))))
-
+    (total-load (cycles array (skip-cycles (cycle-hash array) 1000000000)))))
