87ed7056fe2324d67e1b550369edfcbf32eda4f0
Johan Andersson <nilsjohanandersson@gmail.com>
2024-11-27 21:51:13 +0100

    Day 14 wip

--- 2019/day14.lisp
+++ 2019/day14.lisp
@@ -2,12 +2,27 @@
 
 (in-package :aoc2019.day14)
 
+(defparameter *test*
+"10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL")
+
 (defun parse (input)
   (flet ((read-plist (s)
            (reverse (read-from-string (format nil "(~a)" (aoc:tr "," "  " s))))))
     (mapcar (lambda (line)
               (mapcar #'read-plist (str:split " => " line)))
             (aoc:lines input))))
+
+(defun costs-lookup (costs)
+  (loop with hash = (make-hash-table)
+        for entry in costs
+        for key = (caadr entry)
+        do (setf (gethash key hash) entry)
+        finally (return hash)))
 
 (defun add (bag stuff)
   (cond ((null stuff) bag)
@@ -21,6 +36,45 @@
 
 (defun have? (bag stuff)
   (or (null stuff)
-      (and (<= (gethash (first stuff) bag 0) (second stuff))
+      (and (<= (second stuff) (gethash (first stuff) bag 0))
            (have? bag (cddr stuff))
            bag)))
+
+(defun cost-of (lookup what)
+  (first (gethash what lookup)))
+
+(defun amount-of (lookup what)
+  (second (gethash what lookup)))
+
+(defun add-unstocked (stock needs stuff)
+  (loop for (mat amount) on stuff by #'cddr
+        unless (have? stock (list mat amount))
+          do (add needs (list mat amount))))
+
+(defclass state ()
+  ((stock :reader stock :initform (make-hash-table))
+   (needs :reader needs :initform (add (make-hash-table) '(fuel 1)))
+   (costs :accessor costs :initarg :costs)
+   (mats :accessor mats :initarg :mats)
+   (spent :accessor spent :initform 0)))
+
+(defun make-state (costs)
+  (make-instance 'state
+                 :costs costs
+                 :mats (a:hash-table-keys costs)))
+
+(defun act (state)
+  (with-slots (stock needs mats costs spent) state
+    (dolist (mat mats)
+      (when (gethash mat needs)
+        (let ((cost (cost-of costs mat)))
+          (cond ((have? stock cost)
+                 (take stock cost)
+                 (add stock(amount-of costs mat))
+                 (remhash mat needs))
+                (t
+                 (add-unstocked stock needs cost))))))
+    (incf (gethash 'ore stock 0) (gethash 'ore needs 0))
+    (incf spent (gethash 'ore needs))
+    (setf (gethash 'ore needs) 0))
+  state)
